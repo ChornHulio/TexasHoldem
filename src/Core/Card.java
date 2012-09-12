fce4bd3d9b773cfecfc26a69cd8dad1a0e01bb6c
Dimko <dimko@ubuntu.(none)>
2012-09-12 20:02:26 +0200

    compute card power done

--- src/Core/Card.java
+++ src/Core/Card.java
@@ -1,6 +1,6 @@
 package Core;
 
-public class Card {
+public class Card implements Comparable<Card>{
 
 	public enum SIGN {
 		HEARTS,
@@ -31,5 +31,29 @@ public class Card {
 	public Card(SIGN sign, VALUE value){
 		this.sign = sign;
 		this.value = value;
+	}
+
+	@Override
+	public int compareTo(Card card){
+//		if(sign.ordinal() > card.sign.ordinal())
+//			return 1;
+//		else if(sign.ordinal() < card.sign.ordinal())
+//			return -1;
+		
+		if(value.ordinal() > card.value.ordinal())
+			return 1;
+		else if(value.ordinal() < card.value.ordinal())
+			return -1;
+		return 0;
+	}
+	
+	public String toString(){
+		return "(" + value + "|" + sign + ")";
+	}
+	
+	public static void main(String[] args) {
+		Card c1 = new Card(SIGN.DIAMONDS, VALUE.EIGHT);
+		Card c2 = new Card(SIGN.HEARTS, VALUE.ACE);
+		System.out.println(c2.compareTo(c2));
 	}
 }
